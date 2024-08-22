e2faf2fc18e82fd0b0c8ec86dec38927d1b71d93
kipariss1 <kpassudin@gmail.com>
2024-08-22 16:20:04 +0200

    First sketch of HomeView

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -2,9 +2,31 @@
 </script>
 
 <template>
-  <div>
-    <h1>
-      Welcome to PWN's Token Bundler
-    </h1>
+  <div class="container">
+    <div class='row py-3'>
+      <h1>
+        Welcome to PWN's Token Bundler
+      </h1>
+    </div>
+    <div class="row">
+      <div class="col-7">
+        <h6>
+          <li>Start by connecting your wallet <router-link>here</router-link></li>
+          <li>Or continue by connecting another wallet <router-link>here</router-link></li>
+        </h6>
+      </div>
+      <div class="col-5">
+        <div class="card" style="width: 18rem;">
+          <div class="card-header">
+            List of connected wallets:
+          </div>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">Dummy wallet #1</li>
+            <li class="list-group-item">Dummy wallet #2</li>
+            <li class="list-group-item">Dummy wallet #3</li>
+          </ul>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
