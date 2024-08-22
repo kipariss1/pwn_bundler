99a87397257a3517c75813d678d784cd9763b0e5
kipariss1 <kpassudin@gmail.com>
2024-08-22 16:26:11 +0200

    works!

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,6 +4,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 import bootstrap from 'bootstrap/dist/js/bootstrap.js'
 
 import App from './App.vue'
