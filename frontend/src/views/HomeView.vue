ff99a0e4023f0b82be9b7cfe1d137a3ed1bbf6fa
kipariss1 <kpassudin@gmail.com>
2024-08-22 13:01:38 +0200

    begin of developing frontend

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>
+    Home view blah blah
+  </h1>
 </template>
