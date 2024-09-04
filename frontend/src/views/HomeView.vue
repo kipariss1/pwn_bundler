1b6b6838045df182211d456d56b37445244ddbea
kipariss1 <kpassudin@gmail.com>
2024-09-04 21:06:16 +0200

    fixed imports

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -7,7 +7,7 @@ import {
   WALLET_LIST_FAIL,
   WALLET_LIST_REQUEST,
   WALLET_LIST_SUCCESS, 
-} from 'constatnts/wallet_constants';
+} from '@/../constatnts/wallet_constants';
 
 const walletStore = useWalletsStore()
 const { wallets, type, error } = storeToRefs(walletStore)
